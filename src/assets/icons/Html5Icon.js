85fb02b40cb77aea132dd1a4d5ab1ca1152adba9
Francoveliz <francoleandroveliz@gmail.com>
2021-05-29 17:09:04 -0300

    meta description

--- src/assets/icons/Html5Icon.js
+++ src/assets/icons/Html5Icon.js
@@ -7,8 +7,6 @@ const Html5Icon = () => {
 			id="Layer_1"
 			xmlns="http://www.w3.org/2000/svg"
 			xmlnsXlink="http://www.w3.org/1999/xlink"
-			x="0px"
-			y="0px"
 			viewBox="0 0 512 512"
 			enableBackground="new 0 0 512 512"
 			xmlSpace="preserve">
