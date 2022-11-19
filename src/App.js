a2f84074e05d3ea02681b268dd8c6b08b2e91f9c
Franco Leandro Veliz <franco.veliz@estoes.me>
2022-11-18 21:28:39 -0300

    remodeling

--- src/App.js
+++ src/App.js
@@ -8,16 +8,21 @@ import Skills from "./sections/skills/Skills";
 
 function App() {
 	return (
-		<div
-			className={`min-h-screen text-gray-600 font-sans ${backgroundColor}`}>
-			<Navbar />
-			<main className="flex flex-col pb-32 gap-20 ">
-				<About />
-				<Portfolio />
-				<Skills />
-				<Education />
-				<Idiomas />
-			</main>
+		// <div
+		// 	className={`min-h-screen text-gray-600 font-sans ${backgroundColor}`}>
+		// 	<Navbar />
+		// 	<main className="flex flex-col gap-20 pb-32 ">
+		// 		<About />
+		// 		<Portfolio />
+		// 		<Skills />
+		// 		<Education />
+		// 		<Idiomas />
+		// 	</main>
+		// </div>
+		<div className="flex items-center justify-center w-screen h-screen bg-yellow-400">
+			<p className="text-4xl text-black">
+				Este porfolio se está remodelando, disculpe las molestias.
+			</p>
 		</div>
 	);
 }
